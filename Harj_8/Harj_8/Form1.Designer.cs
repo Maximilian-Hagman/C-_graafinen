44e885ff6f68eb2e1b7e65433edd5ea3f5889a89
max <maximilian.hagman@gmail.com>
2025-02-10 12:59:38 +0200

    Add files via upload

---
+++ Harj_8/Harj_8/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Harj_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TekstiTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.MuunaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TekstiTB
+            // 
+            this.TekstiTB.Location = new System.Drawing.Point(371, 200);
+            this.TekstiTB.Name = "TekstiTB";
+            this.TekstiTB.Size = new System.Drawing.Size(100, 23);
+            this.TekstiTB.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Roomalaiset numerot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Anna numero välillä 1-3999, niin muutan sen roomalaiseksi:";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(81, 337);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(38, 15);
+            this.VastausLB.TabIndex = 3;
+            this.VastausLB.Text = "label3";
+            this.VastausLB.Visible = false;
+            // 
+            // MuunaBT
+            // 
+            this.MuunaBT.Location = new System.Drawing.Point(396, 337);
+            this.MuunaBT.Name = "MuunaBT";
+            this.MuunaBT.Size = new System.Drawing.Size(75, 23);
+            this.MuunaBT.TabIndex = 4;
+            this.MuunaBT.Text = "Muuta";
+            this.MuunaBT.UseVisualStyleBackColor = true;
+            this.MuunaBT.Click += new System.EventHandler(this.MuunaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MuunaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TekstiTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox TekstiTB;
+        private Label label1;
+        private Label label2;
+        private Label VastausLB;
+        private Button MuunaBT;
+    }
+}
