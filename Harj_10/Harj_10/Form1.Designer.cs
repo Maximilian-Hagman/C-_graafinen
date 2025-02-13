40e4c1cf64a8624f483f2e7a96e629759f1600c1
max <maximilian.hagman@gmail.com>
2025-02-13 09:53:46 +0200

    Add files via upload

---
+++ Harj_10/Harj_10/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Harj_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kuvausLB = new System.Windows.Forms.Label();
+            this.BmiLB = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BMIBT = new System.Windows.Forms.Button();
+            this.painoTB = new System.Windows.Forms.TextBox();
+            this.pituusTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // kuvausLB
+            // 
+            this.kuvausLB.AutoSize = true;
+            this.kuvausLB.Location = new System.Drawing.Point(12, 107);
+            this.kuvausLB.Name = "kuvausLB";
+            this.kuvausLB.Size = new System.Drawing.Size(52, 15);
+            this.kuvausLB.TabIndex = 0;
+            this.kuvausLB.Text = "Vastaus2";
+            this.kuvausLB.Visible = false;
+            // 
+            // BmiLB
+            // 
+            this.BmiLB.AutoSize = true;
+            this.BmiLB.Location = new System.Drawing.Point(12, 92);
+            this.BmiLB.Name = "BmiLB";
+            this.BmiLB.Size = new System.Drawing.Size(46, 15);
+            this.BmiLB.TabIndex = 1;
+            this.BmiLB.Text = "Vastaus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Anna paino:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Anna pituus:";
+            // 
+            // BMIBT
+            // 
+            this.BMIBT.Location = new System.Drawing.Point(12, 66);
+            this.BMIBT.Name = "BMIBT";
+            this.BMIBT.Size = new System.Drawing.Size(187, 23);
+            this.BMIBT.TabIndex = 4;
+            this.BMIBT.Text = "Laske painoindeksi";
+            this.BMIBT.UseVisualStyleBackColor = true;
+            this.BMIBT.Click += new System.EventHandler(this.BMIBT_Click);
+            // 
+            // painoTB
+            // 
+            this.painoTB.Location = new System.Drawing.Point(99, 6);
+            this.painoTB.Name = "painoTB";
+            this.painoTB.Size = new System.Drawing.Size(100, 23);
+            this.painoTB.TabIndex = 5;
+            // 
+            // pituusTB
+            // 
+            this.pituusTB.Location = new System.Drawing.Point(99, 37);
+            this.pituusTB.Name = "pituusTB";
+            this.pituusTB.Size = new System.Drawing.Size(100, 23);
+            this.pituusTB.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pituusTB);
+            this.Controls.Add(this.painoTB);
+            this.Controls.Add(this.BMIBT);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BmiLB);
+            this.Controls.Add(this.kuvausLB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label kuvausLB;
+        private Label BmiLB;
+        private Label label3;
+        private Label label4;
+        private Button BMIBT;
+        private TextBox painoTB;
+        private TextBox pituusTB;
+    }
+}
