852496a8ece7c4d413cd82d214b077b799a80880
max <maximilian.hagman@gmail.com>
2025-02-13 09:08:43 +0200

    Add files via upload

---
+++ Harj_9/Harj_9/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Harj_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.CelsiusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(12, 85);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(46, 15);
+            this.VastausLB.TabIndex = 0;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna asteet:";
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.Location = new System.Drawing.Point(90, 30);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(100, 23);
+            this.AsteetTB.TabIndex = 2;
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.Location = new System.Drawing.Point(304, 30);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(75, 23);
+            this.MuunnaBT.TabIndex = 3;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = true;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // CelsiusRB
+            // 
+            this.CelsiusRB.AutoSize = true;
+            this.CelsiusRB.Location = new System.Drawing.Point(204, 26);
+            this.CelsiusRB.Name = "CelsiusRB";
+            this.CelsiusRB.Size = new System.Drawing.Size(62, 19);
+            this.CelsiusRB.TabIndex = 4;
+            this.CelsiusRB.TabStop = true;
+            this.CelsiusRB.Text = "Celsius";
+            this.CelsiusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Location = new System.Drawing.Point(204, 51);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(81, 19);
+            this.FahrenheitRB.TabIndex = 5;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Miten muunnat?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 112);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FahrenheitRB);
+            this.Controls.Add(this.CelsiusRB);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.VastausLB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label VastausLB;
+        private Label label2;
+        private TextBox AsteetTB;
+        private Button MuunnaBT;
+        private RadioButton CelsiusRB;
+        private RadioButton FahrenheitRB;
+        private Label label3;
+    }
+}
---
+++ Harj_9/Harj_9/Form1.cs
@@ -0,0 +1,33 @@
+namespace Harj_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MuunnaBT_Click(object sender, EventArgs e)
+        {
+            double vastaus;
+            double asteet = Convert.ToDouble(AsteetTB.Text);
+            if(CelsiusRB.Checked)
+            {
+                vastaus = asteet * 1.8 + 32;
+                VastausLB.Text = asteet + " Celcista on " + vastaus + " Fahrenheit astetta";
+                VastausLB.Visible = true;
+            }
+            else if(FahrenheitRB.Checked)
+            {
+                vastaus = (asteet - 32) / 1.8;
+                VastausLB.Text = asteet + " Celciusta on " + vastaus + " Fahrenheit astetta";
+                VastausLB.Visible = true;
+            }
+            else
+            {
+                VastausLB.Text = "Et valinnut muunnettavaa astetta tai antanut oikeaa lukua";
+                VastausLB.Visible = true;
+            }
+        }
+    }
+}
