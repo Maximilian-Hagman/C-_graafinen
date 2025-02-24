46c079f0d6ca9af9391fce58e7c15521c9ba03ce
max <maximilian.hagman@gmail.com>
2025-02-24 10:25:03 +0200

    Add files via upload

---
+++ Harj_12/Harj_12/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Harj_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KysymysLB = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DRB = new System.Windows.Forms.RadioButton();
+            this.CRB = new System.Windows.Forms.RadioButton();
+            this.BRB = new System.Windows.Forms.RadioButton();
+            this.ARB = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // KysymysLB
+            // 
+            this.KysymysLB.AutoSize = true;
+            this.KysymysLB.Location = new System.Drawing.Point(12, 9);
+            this.KysymysLB.Name = "KysymysLB";
+            this.KysymysLB.Size = new System.Drawing.Size(134, 15);
+            this.KysymysLB.TabIndex = 0;
+            this.KysymysLB.Text = "Vastaus 1. kysymykseen:";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(12, 138);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(46, 15);
+            this.VastausLB.TabIndex = 1;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.DRB);
+            this.panel1.Controls.Add(this.CRB);
+            this.panel1.Controls.Add(this.BRB);
+            this.panel1.Controls.Add(this.ARB);
+            this.panel1.Location = new System.Drawing.Point(152, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(58, 141);
+            this.panel1.TabIndex = 2;
+            // 
+            // DRB
+            // 
+            this.DRB.AutoSize = true;
+            this.DRB.Location = new System.Drawing.Point(15, 78);
+            this.DRB.Name = "DRB";
+            this.DRB.Size = new System.Drawing.Size(33, 19);
+            this.DRB.TabIndex = 3;
+            this.DRB.TabStop = true;
+            this.DRB.Text = "D";
+            this.DRB.UseVisualStyleBackColor = true;
+            // 
+            // CRB
+            // 
+            this.CRB.AutoSize = true;
+            this.CRB.Location = new System.Drawing.Point(16, 53);
+            this.CRB.Name = "CRB";
+            this.CRB.Size = new System.Drawing.Size(33, 19);
+            this.CRB.TabIndex = 2;
+            this.CRB.TabStop = true;
+            this.CRB.Text = "C";
+            this.CRB.UseVisualStyleBackColor = true;
+            // 
+            // BRB
+            // 
+            this.BRB.AutoSize = true;
+            this.BRB.Location = new System.Drawing.Point(15, 28);
+            this.BRB.Name = "BRB";
+            this.BRB.Size = new System.Drawing.Size(32, 19);
+            this.BRB.TabIndex = 1;
+            this.BRB.TabStop = true;
+            this.BRB.Text = "B";
+            this.BRB.UseVisualStyleBackColor = true;
+            // 
+            // ARB
+            // 
+            this.ARB.AutoSize = true;
+            this.ARB.Location = new System.Drawing.Point(15, 3);
+            this.ARB.Name = "ARB";
+            this.ARB.Size = new System.Drawing.Size(33, 19);
+            this.ARB.TabIndex = 0;
+            this.ARB.TabStop = true;
+            this.ARB.Text = "A";
+            this.ARB.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 162);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.KysymysLB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label KysymysLB;
+        private Label VastausLB;
+        private Panel panel1;
+        private RadioButton DRB;
+        private RadioButton CRB;
+        private RadioButton BRB;
+        private RadioButton ARB;
+    }
+}
