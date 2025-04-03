025c127368d835809a2396183f502ce3a5aaecc6
max <maximilian.hagman@gmail.com>
2025-04-03 11:26:23 +0300

    Add files via upload

---
+++ Harj_2/Harj_2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Harj_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TulostusLB = new System.Windows.Forms.Label();
+            this.ViestiTB = new System.Windows.Forms.TextBox();
+            this.TulostaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna tulostettava teksti:";
+            // 
+            // TulostusLB
+            // 
+            this.TulostusLB.AutoSize = true;
+            this.TulostusLB.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TulostusLB.ForeColor = System.Drawing.Color.MintCream;
+            this.TulostusLB.Location = new System.Drawing.Point(12, 42);
+            this.TulostusLB.Name = "TulostusLB";
+            this.TulostusLB.Size = new System.Drawing.Size(72, 33);
+            this.TulostusLB.TabIndex = 1;
+            this.TulostusLB.Text = "xxxx";
+            this.TulostusLB.Visible = false;
+            // 
+            // ViestiTB
+            // 
+            this.ViestiTB.Location = new System.Drawing.Point(306, 17);
+            this.ViestiTB.Name = "ViestiTB";
+            this.ViestiTB.Size = new System.Drawing.Size(123, 23);
+            this.ViestiTB.TabIndex = 2;
+            // 
+            // TulostaBT
+            // 
+            this.TulostaBT.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TulostaBT.Location = new System.Drawing.Point(306, 46);
+            this.TulostaBT.Name = "TulostaBT";
+            this.TulostaBT.Size = new System.Drawing.Size(123, 33);
+            this.TulostaBT.TabIndex = 3;
+            this.TulostaBT.Text = "Tulosta teksti";
+            this.TulostaBT.UseVisualStyleBackColor = true;
+            this.TulostaBT.Click += new System.EventHandler(this.TulostaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(488, 81);
+            this.Controls.Add(this.TulostaBT);
+            this.Controls.Add(this.ViestiTB);
+            this.Controls.Add(this.TulostusLB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label TulostusLB;
+        private TextBox ViestiTB;
+        private Button TulostaBT;
+    }
+}
