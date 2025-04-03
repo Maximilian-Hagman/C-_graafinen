4ead65aea0ba0bb090d9b583d1cb1e29678eb591
max <maximilian.hagman@gmail.com>
2025-04-03 09:26:07 +0300

    Add files via upload

---
+++ Harj_3/Harj_3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Harj_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.LaskutoimitusCB = new System.Windows.Forms.ComboBox();
+            this.LukuYksiTB = new System.Windows.Forms.TextBox();
+            this.LukuKaksiTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(234, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "=";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.VastausLB.Location = new System.Drawing.Point(268, 18);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(26, 30);
+            this.VastausLB.TabIndex = 1;
+            this.VastausLB.Text = "X";
+            this.VastausLB.Visible = false;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LaskeBT.Location = new System.Drawing.Point(300, 12);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(88, 35);
+            this.LaskeBT.TabIndex = 2;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // LaskutoimitusCB
+            // 
+            this.LaskutoimitusCB.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LaskutoimitusCB.FormattingEnabled = true;
+            this.LaskutoimitusCB.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.LaskutoimitusCB.Location = new System.Drawing.Point(103, 18);
+            this.LaskutoimitusCB.Name = "LaskutoimitusCB";
+            this.LaskutoimitusCB.Size = new System.Drawing.Size(31, 23);
+            this.LaskutoimitusCB.TabIndex = 3;
+            // 
+            // LukuYksiTB
+            // 
+            this.LukuYksiTB.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LukuYksiTB.Location = new System.Drawing.Point(12, 12);
+            this.LukuYksiTB.Name = "LukuYksiTB";
+            this.LukuYksiTB.Size = new System.Drawing.Size(85, 36);
+            this.LukuYksiTB.TabIndex = 4;
+            // 
+            // LukuKaksiTB
+            // 
+            this.LukuKaksiTB.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LukuKaksiTB.Location = new System.Drawing.Point(140, 12);
+            this.LukuKaksiTB.Name = "LukuKaksiTB";
+            this.LukuKaksiTB.Size = new System.Drawing.Size(88, 36);
+            this.LukuKaksiTB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 106);
+            this.Controls.Add(this.LukuKaksiTB);
+            this.Controls.Add(this.LukuYksiTB);
+            this.Controls.Add(this.LaskutoimitusCB);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label VastausLB;
+        private Button LaskeBT;
+        private ComboBox LaskutoimitusCB;
+        private TextBox LukuYksiTB;
+        private TextBox LukuKaksiTB;
+    }
+}
