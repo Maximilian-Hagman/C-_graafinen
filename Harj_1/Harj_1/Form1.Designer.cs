437d9b35c5f8cff7139566bfaff132ec5fdf4f32
max <maximilian.hagman@gmail.com>
2025-04-03 10:04:42 +0300

    Add files via upload

---
+++ Harj_1/Harj_1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Harj_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.VaihdaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Location = new System.Drawing.Point(36, 9);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(47, 15);
+            this.OtsikkoLB.TabIndex = 0;
+            this.OtsikkoLB.Text = "Otsikko";
+            // 
+            // VaihdaBT
+            // 
+            this.VaihdaBT.Location = new System.Drawing.Point(21, 27);
+            this.VaihdaBT.Name = "VaihdaBT";
+            this.VaihdaBT.Size = new System.Drawing.Size(87, 41);
+            this.VaihdaBT.TabIndex = 1;
+            this.VaihdaBT.Text = "Vaihda teksti";
+            this.VaihdaBT.UseVisualStyleBackColor = true;
+            this.VaihdaBT.Click += new System.EventHandler(this.VaihdaBT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 73);
+            this.Controls.Add(this.VaihdaBT);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Name = "Form1";
+            this.Text = resources.GetString("$this.Text");
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label OtsikkoLB;
+        private Button VaihdaBT;
+    }
+}
